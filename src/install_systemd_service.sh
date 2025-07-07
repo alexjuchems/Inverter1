bc354919a02bbd661b07781bcc3b9361220c081e
alexjuchems <aljuchems@gmail.com>
2025-07-07 22:05:51 +0200

    Update install_systemd_service.sh

--- src/install_systemd_service.sh
+++ src/install_systemd_service.sh
@@ -12,24 +12,24 @@ After=multi-user.target
 Type=simple
 User=USER
 WorkingDirectory=BASE_DIR
-ExecStart=BASE_DIR/python3 mtec_mqtt
+ExecStart=BASE_DIR/python3 mtec_mqtt1
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
 "
 
-echo "MTECmqtt: Installing systemd service to auto-start mtec_mqtt"
+echo "MTECmqtt: Installing systemd service to auto-start mtec_mqtt1"
 
 if [ $(id -u) != "0" ]; then
   echo "This script required root rights. Please restart using 'sudo'"
 else
-  echo "$SVC_TXT" | sed "s!BASE_DIR!$BASE_DIR!g" | sed "s/USER/$SUDO_USER/g" > /tmp/mtec_mqtt.service 
-  chmod 666 /tmp/mtec_mqtt.service
-  mv /tmp/mtec_mqtt.service /etc/systemd/system
+  echo "$SVC_TXT" | sed "s!BASE_DIR!$BASE_DIR!g" | sed "s/USER/$SUDO_USER/g" > /tmp/mtec_mqtt1.service 
+  chmod 666 /tmp/mtec_mqtt1.service
+  mv /tmp/mtec_mqtt1.service /etc/systemd/system
   systemctl daemon-reload
-  systemctl enable mtec_mqtt.service
-  systemctl start mtec_mqtt.service
-  echo "==> systemd service '/etc/systemd/system/mtec_mqtt.service' installed"
+  systemctl enable mtec_mqtt1.service
+  systemctl start mtec_mqtt1.service
+  echo "==> systemd service '/etc/systemd/system/mtec_mqtt1.service' installed"
 fi
 
